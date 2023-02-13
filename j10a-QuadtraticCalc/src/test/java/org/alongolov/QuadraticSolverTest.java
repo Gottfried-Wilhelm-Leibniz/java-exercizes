517f9e04f1fd96f51361d379d388ec0e30b6064b
Alon Golov <golovmania@gmail.com>
2023-02-13 16:35:50 +0200

    after phase 2

---
+++ j10a-QuadtraticCalc/src/test/java/org/alongolov/QuadraticSolverTest.java
@@ -0,0 +1,39 @@
+package org.alongolov;
+import org.junit.jupiter.api.Assertions;
+
+class QuadraticSolverTest {
+    @org.junit.jupiter.api.Test
+    void solve0() {
+        var s = new QuadraticSolver();
+        var answer = s.solve(1,0,4);
+        Assertions.assertTrue(answer.size() == 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void solve1() {
+        var s = new QuadraticSolver();
+        var answer = s.solve(1,0,-4);
+        answer = s.solve(1,2,1);
+        Assertions.assertTrue(answer.size() == 1);
+        Assertions.assertEquals(-1, answer.get(0), 0.0001);
+
+        Assertions.assertTrue(answer.size() == 2);
+        Assertions.assertEquals(2, answer.get(0), 0.0001);
+        Assertions.assertEquals(-2, answer.get(1), 0.0001);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void solve2() {
+        var s = new QuadraticSolver();
+        var answer = s.solve(1,0,-4);
+        answer = s.solve(1,2,1);
+        Assertions.assertTrue(answer.size() == 1);
+        Assertions.assertEquals(-1, answer.get(0), 0.0001);
+        var answer = ui.uiIt(1,0,-4);
+        Assertions.assertTrue(answer.size() == 2);
+        Assertions.assertEquals(2, answer.get(0), 0.0001);
+        Assertions.assertEquals(-2, answer.get(1), 0.0001);
+
+    }
+}
